18e40b5baee05c5e14595ef6f676b94d46a29d63
Ben Dudson <bd512@york.ac.uk>
2018-10-22 16:12:36 +0100

    Trying another method
    Suggested here:
    https://docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,15 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-autodoc_mock_imports = ['_tkinter']
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['_tkinter']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
