f81427ac4695bad01d58f26d9c6727135d65206d
Ben Dudson <bd512@york.ac.uk>
2019-11-18 17:09:39 +0000

    Add tests of Laplacian solvers
    Direct and multigrid solver, comparison against generated solution,
    using forward and reverse operations.

--- freegs/test_linearsolve.py
+++ freegs/test_linearsolve.py
@@ -2,19 +2,72 @@
 Tests of the linear solver
 """
 
-import numpy as np
+from . import multigrid
 
+import numpy as np
 
 ###### Laplacian in 2D
 
-
-def run():
+def test_direct_laplacian():
     nx = 65
     ny = 65
 
-    dx = 1./(nx - 1)
-    dy = 1./(ny - 1)
+    Lx = 2.
+    Ly = 3.
     
-    xx, yy = meshgrid(np.linspace(0,1,nx), np.linspace(0,1,ny))
-    
+    dx = Lx/(nx - 1)
+    dy = Ly/(ny - 1)
+
+    xx, yy = np.meshgrid(np.linspace(0,Lx,nx), np.linspace(0,Ly,ny))
+
     solution = np.exp( - ( (xx - 0.5)**2 + (yy - 0.5)**2 ) / 0.4**2 )
+    
+    A = multigrid.LaplacianOp()
+    rhs = A(solution, dx, dy)
+
+    # Copy boundaries
+    rhs[:,0] = solution[:,0]
+    rhs[0,:] = solution[0,:]
+    rhs[:,-1] = solution[:,-1]
+    rhs[-1,:] = solution[-1,:]
+    
+    solve = multigrid.MGDirect(multigrid.LaplaceSparse(Lx, Ly)(nx, ny))
+    x = solve(None, rhs)
+    
+    assert np.allclose(x, solution)
+
+
+def test_multigrid_laplacian():
+    nx = 65
+    ny = 65
+
+    Lx = 2.
+    Ly = 3.
+    
+    dx = Lx/(nx - 1)
+    dy = Ly/(ny - 1)
+
+    xx, yy = np.meshgrid(np.linspace(0,Lx,nx), np.linspace(0,Ly,ny))
+
+    solution = np.exp( - ( (xx - 0.5)**2 + (yy - 0.5)**2 ) / 0.4**2 )
+    
+    A = multigrid.LaplacianOp()
+    rhs = A(solution, dx, dy)
+
+    # Copy boundaries
+    rhs[:,0] = solution[:,0]
+    rhs[0,:] = solution[0,:]
+    rhs[:,-1] = solution[:,-1]
+    rhs[-1,:] = solution[-1,:]
+    
+    solve = multigrid.createVcycle(nx,ny, multigrid.LaplaceSparse(Lx, Ly),
+                                   ncycle=50, niter=50, nlevels=4, direct=True)
+
+    xinput = np.zeros(rhs.shape)
+    xinput[:,0] = solution[:,0]
+    xinput[0,:] = solution[0,:]
+    xinput[:,-1] = solution[:,-1]
+    xinput[-1,:] = solution[-1,:]
+    x = solve(xinput, rhs)
+    
+    assert np.allclose(x, solution, atol=1e-6)
