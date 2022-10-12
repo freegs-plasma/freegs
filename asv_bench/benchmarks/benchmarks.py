c5420d1ec8ed9920111de2290e38270ce87dd7eb
Peter Hill <peter.hill@york.ac.uk>
2022-10-12 15:59:16 +0100

    Simple benchmark that works with (most) previous versions

--- asv_bench/benchmarks/benchmarks.py
+++ asv_bench/benchmarks/benchmarks.py
@@ -1,9 +1,25 @@
-from freegs import geqdsk
-from freegs import machine
+import freegs
 
 
-def time_read_geqdsk():
-    tokamak = machine.TestTokamak()
+def time_freeboundary():
+    eq = freegs.Equilibrium(
+        tokamak=freegs.machine.TestTokamak(),
+        Rmin=0.1,
+        Rmax=2.0,
+        Zmin=-1.0,
+        Zmax=1.0,
+        nx=65,
+        ny=65,
+        boundary=freegs.boundary.freeBoundaryHagenow,
+    )
 
-    with open("lsn.geqdsk") as f:
-        geqdsk.read(f, tokamak, show=False)
+    # Signature changed in 0.7.0
+    if freegs.__version__ >= "0.7.0":
+        profiles = freegs.jtor.ConstrainPaxisIp(eq, 1e3, 2e5, 2.0)
+    else:
+        profiles = freegs.jtor.ConstrainPaxisIp(1e3, 2e5, 2.0)
+
+    xpoints = [(1.1, -0.6), (1.1, 0.8)]
+    isoflux = [(1.1, -0.6, 1.1, 0.6)]
+    constrain = freegs.control.constrain(xpoints=xpoints, isoflux=isoflux)
+    freegs.solve(eq, profiles, constrain)
